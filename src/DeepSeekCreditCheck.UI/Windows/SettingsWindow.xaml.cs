b1d7ee4aebedfa647ad1f56ff0812c09ca29a6ea
Lukáš Hanák <hanys.cz@gmail.com>
2026-06-07 17:58:28 +0200

    fix: remove PasswordBox binding, use PasswordChanged event instead
    PasswordBox.Password is a CLR property, not a DependencyProperty,
    so XAML data binding does not work on it. Fixed by:
    - Removing Password binding from XAML
    - Setting Password in code-behind after LoadAsync
    - Using PasswordChanged event to push changes back to ViewModel

--- src/DeepSeekCreditCheck.UI/Windows/SettingsWindow.xaml.cs
+++ src/DeepSeekCreditCheck.UI/Windows/SettingsWindow.xaml.cs
@@ -5,10 +5,24 @@ namespace DeepSeekCreditCheck.UI.Windows;
 
 public partial class SettingsWindow : Window
 {
+    private readonly SettingsViewModel _viewModel;
+
     public SettingsWindow(SettingsViewModel viewModel)
     {
         InitializeComponent();
-        DataContext = viewModel;
-        Loaded += async (_, _) => await viewModel.LoadAsync();
+        _viewModel = viewModel;
+        DataContext = _viewModel;
+
+        Loaded += async (_, _) =>
+        {
+            await _viewModel.LoadAsync();
+            // PasswordBox nemá DP vlastnost, nastavíme ručně po load
+            ApiKeyBox.Password = _viewModel.ApiKey;
+        };
+    }
+
+    private void ApiKeyBox_PasswordChanged(object sender, RoutedEventArgs e)
+    {
+        _viewModel.ApiKey = ApiKeyBox.Password;
     }
 }
